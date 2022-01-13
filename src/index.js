6b669902063fdb2db5098738ea1679ba237e4a54
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-01-13 00:00:51 -0300

    my first website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+// https://www.youtube.com/watch?v=I2UBjN5ER4s&t=659s
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
